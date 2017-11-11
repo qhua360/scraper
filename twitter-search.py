dcb1059b7197e398e0370e1d291fb454d44024e1
qiuyi <qiuyi.hua@uwaterloo.ca>
2017-11-10 19:54:33 -0500

    Add iteration

--- twitter-search.py
+++ twitter-search.py
@@ -17,11 +17,16 @@ time.sleep(1)
 
 body = browser.find_element_by_tag_name('body')
 
-for _ in range(5):
-    body.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
+while True:
+    for _ in range(5):
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
 
-tweets = browser.find_elements_by_class_name('tweet-text')
+    tweets = browser.find_elements_by_class_name('tweet-text')
 
-for tweet in tweets:
-    print(tweet.text)
+    for tweet in tweets:
+        print(tweet.text)
+    
+    it = input("Type 'it' for more tweets.\n")
+    if ("it" != it):
+        break;
